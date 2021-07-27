c06163d94e73feb0d03a2f81cf8d1855def35e5b
AaEzha <aaezha@gmail.com>
2021-07-27 10:57:32 +0700

    fix #1

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -29,8 +29,41 @@ class Dashboard extends CI_Controller
             $data['rows'] = $this->transaksi_model->getDataPerTanggalAdmin();
         }
 
+		// divisi
+		$sql = "select divisi, count(user_id) jumlah from users where divisi is not null group by divisi order by divisi asc";
+		$res = $this->db->query($sql);
+		$divisi_name = "";
+		$divisi_jumlah = "";
+		foreach ($res->result() as $r) {
+			$divisi_name .= "'".$r->divisi ."', ";
+			$divisi_jumlah .= $r->jumlah .", ";
+		}
+		$data['div_name'] = rtrim($divisi_name, ", ");
+		$data['div_jum'] = rtrim($divisi_jumlah, ", ");
+		// end of divisi
 
+		// barang
+		$sql = "Select DATE_FORMAT(created,'%Y-%m') periode, sum(jumlah) jumlah from transaksi group by DATE_FORMAT(created,'%Y-%m') order by created asc";
+		$res = $this->db->query($sql);
+		$barang_periode = "";
+		$barang_jumlah = "";
+		foreach ($res->result() as $r) {
+			$barang_periode .= "'".$r->periode ."', ";
+			$barang_jumlah .= $r->jumlah .", ";
+		}
+		$data['bar_periode'] = rtrim($barang_periode, ", ");
+		$data['bar_jum'] = rtrim($barang_jumlah, ", ");
+		// end of barang
+
+		$data['js'] = 'dashboard/js';
         $data['title'] = "Halaman Dashboard";
+
+		// tipe diagram untuk divisi
+		// kalau chart doughnut, paling baik minimal di angka 60 sampai 100
+		// kalau pie, beri nilai 0
+		$data['ukuran'] = 0; 
+
+
         $this->template->load('template', 'dashboard', $data);
     }
     
--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -1,136 +1,103 @@
 <h1 class="h3 mb-4 text-gray-800">Dashboard</h1>
 <?php if ($this->fungsi->user_login()->level == 1) { ?>
-<div class="row">
+	<div class="row">
 
-<!-- Earnings (Monthly) Card Example -->
-<div class="col-xl-6 col-md-6 mb-4 ">
-    <div class="card border-left-primary shadow h-100 py-2">
-        <div class="card-body">
-            <div class="row no-gutters align-items-center">
-                <div class="col mr-2">
-                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Data Pengambilan Barang Hari ini</div>
+		<!-- Earnings (Monthly) Card Example -->
+		<div class="col-xl-6 col-md-6 mb-4 ">
+			<div class="card border-left-primary shadow h-100 py-2">
+				<div class="card-body">
+					<div class="row no-gutters align-items-center">
+						<div class="col mr-2">
+							<div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Data Pengambilan Barang Hari ini</div>
 
-                    <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $rows; ?></div>
+							<div class="h5 mb-0 font-weight-bold text-gray-800"><?= $rows; ?></div>
 
-                </div>
-                <div class="col-auto">
-                    <i class="fas fa-shopping-cart fa-2x text-gray-300"></i>
+						</div>
+						<div class="col-auto">
+							<i class="fas fa-shopping-cart fa-2x text-gray-300"></i>
 
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 
-<div class="col-xl-6 col-md-6 mb-4 ">
-    <div class="card border-left-success shadow h-100 py-2">
-        <a href="<?= base_url('transaksi') ?>">
-            <div class="card-body">
-                <div class="row no-gutters align-items-center">
-                    <div class="col mr-2">
+		<div class="col-xl-6 col-md-6 mb-4 ">
+			<div class="card border-left-success shadow h-100 py-2">
+				<a href="<?= base_url('transaksi') ?>">
+					<div class="card-body">
+						<div class="row no-gutters align-items-center">
+							<div class="col mr-2">
 
-                        <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Total Data Pengambilan Barang </div>
+								<div class="text-xs font-weight-bold text-success text-uppercase mb-1">Total Data Pengambilan Barang </div>
 
-                        <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $total; ?></div>
-                    </div>
-                    <div class="col-auto">
+								<div class="h5 mb-0 font-weight-bold text-gray-800"><?= $total; ?></div>
+							</div>
+							<div class="col-auto">
 
-                        <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+								<i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
 
-                    </div>
-                </div>
-            </div>
-        </a>
-    </div>
-</div>
+							</div>
+						</div>
+					</div>
+				</a>
+			</div>
+		</div>
 
-<!-- Conten Row -->
-<div class="row">
-    <!-- Area Chart -->
-    <div class="col-xl-8 col-lg-7">
-        <div class="card shadow mb-4">
-           <!-- card header - dropdown -->
-           <div class="card-header py-3 d-flex flex-row align-items-center justifiy-content-between">
-               <h6 class="m-0 font-weight-bold text-primary">Data Pengambilan Barang</h6>
-               <div class="dropdown no-arrow">
-                    <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true">
-                       <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
-                        <div class="dropdown-header">Dropdown Header:</div>
-                        <a class="dropdown-item" href="#">Action</a>
-                        <a class="dropdown-item" href="#">Another action</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Something else here</a>
-                </div>
-            </div>
-        </div>
-        <!-- card body -->
-        <div class="card-body">
-            <div class="chart-area">
-                <canvas id="myAreaChart"></canvas>
-            </div>
-        </div>
-        </div>
-    </div>
+	</div>
 
-    <!-- pie chart -->
-    <div class="col-xl-4 col-lg-5">
-        <div class="card shadow mb-4">
-        <!-- card header - dropdown -->
-        <div class="card-header py-3 d-flex flex-row align-items-center justifiy-content-between">
-               <h6 class="m-0 font-weight-bold text-primary">Divisi</h6>
-               <div class="dropdown no-arrow">
-                    <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true">
-                       <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
-                        <div class="dropdown-header">Dropdown Header:</div>
-                        <a class="dropdown-item" href="#">Action</a>
-                        <a class="dropdown-item" href="#">Another action</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Something else here</a>
-                </div>
-            </div>
-        </div>
-        <!-- card body -->
-        <div class="card-body">
-            <div class="chart-pie pt-4 pb-2">
-                <canvas id="myPieChart"></canvas>
-            </div>
-            <div class="mt-4 text-center small">
-                <span class="mr-2">
-                    <i class="fas fa-circle text-primary"></i>Manajemen Pemasaran
-                </span>
-                <span class="mr-2">
-                    <i class="fas fa-circle text-primary"></i>Manajemen Kontrak dan Lisensi 
-                </span>
-                <span class="mr-2">
-                    <i class="fas fa-circle text-primary"></i>Manajemen Kontrak
-                </span>
-            </div>
-            </div>
-        </div>                  
-</div>
+	<!-- Conten Row -->
+	<div class="row">
+		<!-- Area Chart -->
+		<div class="col-xl-8 col-lg-7">
+			<div class="card shadow mb-4">
+				<!-- card header - dropdown -->
+				<div class="card-header py-3 d-flex flex-row align-items-center justifiy-content-between">
+					<h6 class="m-0 font-weight-bold text-primary">Data Pengambilan Barang</h6>
+				</div>
+				<!-- card body -->
+				<div class="card-body">
+					<div class="chart-area">
+						<canvas id="myBarChart"></canvas>
+					</div>
+				</div>
+			</div>
+		</div>
 
-<?php } ?>
-<?php if ($this->fungsi->user_login()->level == 2) { ?>
-<h2 class="text-center">Halo <?= $this->fungsi->user_login()->name ?>!</h2>
-<h2 class="text-center">Silahkan Input Data Barangnya Disini</h2> 
-</br>
-<div class="row"> 
-    <div class="col-12 text-center">
-        <a href="<?= site_url('transaksi/tambah') ?>" class="btn btn-secondary btn-lg"><i class="fas fa-barang-plus"></i> Input Pengambilan Barang</a>
-    </div>
-</div>
-</div>
-        <div class="row">
-          <img src="<?= base_url('assets/'); ?>img/Checklist.jpg" class="img-fluid" alt="..."> 
-        
+		<!-- pie chart -->
+		<div class="col-xl-4 col-lg-5">
+			<div class="card shadow mb-4">
+				<!-- card header - dropdown -->
+				<div class="card-header py-3 d-flex flex-row align-items-center justifiy-content-between">
+					<h6 class="m-0 font-weight-bold text-primary">Divisi</h6>
+				</div>
+				<!-- card body -->
+				<div class="card-body">
+					<div class="chart-pie pt-4">
+						<canvas id="myPieChart"></canvas>
+					</div>
+				</div>
+			</div>
+		</div>
 
-<?php } ?> 
+	<?php } ?>
+	<?php if ($this->fungsi->user_login()->level == 2) { ?>
+		<h2 class="text-center">Halo <?= $this->fungsi->user_login()->name ?>!</h2>
+		<h2 class="text-center">Silahkan Input Data Barangnya Disini</h2>
+		</br>
+		<div class="row">
+			<div class="col-12 text-center">
+				<a href="<?= site_url('transaksi/tambah') ?>" class="btn btn-secondary btn-lg"><i class="fas fa-barang-plus"></i> Input Pengambilan Barang</a>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<img src="<?= base_url('assets/'); ?>img/Checklist.jpg" class="img-fluid" alt="...">
 
 
-<!-- Content Row -->
+	<?php } ?>
 
-<!-- End of Main Content -->
+
+	<!-- Content Row -->
+
+	<!-- End of Main Content -->
